1b9e496acb7247f4248cc3cc146bade34bad7339
MikeDiaz1 <diaz-@live.ca>
2025-01-20 16:04:21 -0800

    fix bug with home nav and added image effect to pubs page

--- _pages/publications.md
+++ _pages/publications.md
@@ -8,6 +8,24 @@ permalink: /publications/
 <div class="publications-hero-container">
 </div>
 
+<div class="front-effect-container">
+    <img class="image" data-index="0" data-status="inactive" src="../img/front_effect/OvCa-Zoom.jpg" />
+    <img class="image" data-index="1" data-status="inactive" src="../img/front_effect/OvCa-Zoom7.jpg" />
+    <img class="image" data-index="2" data-status="inactive" src="../img/front_effect/OvCa-Zoom6.jpg" />
+    <img class="image" data-index="3" data-status="inactive" src="../img/front_effect/OvCa-Zoom8.jpg" />
+    <img class="image" data-index="4" data-status="inactive" src="../img/front_effect/OvCa-Zoom12.jpg" />
+    <img class="image" data-index="5" data-status="inactive" src="../img/front_effect/OvCa-Zoom9.jpg" />
+    <img class="image" data-index="6" data-status="inactive" src="../img/front_effect/OvCa-Zoom13.jpg" />
+    <img class="image" data-index="7" data-status="inactive" src="../img/front_effect/OvCa-Zoom15.jpg" />
+    <img class="image" data-index="8" data-status="inactive" src="../img/front_effect/OvCa-Zoom3.jpg" />
+    <img class="image" data-index="9" data-status="inactive" src="../img/front_effect/OvCa-Zoom11.jpg" />
+    <img class="image" data-index="10" data-status="inactive" src="../img/front_effect/OvCa-Zoom10.jpg" />
+    <img class="image" data-index="11" data-status="inactive" src="../img/front_effect/OvCa-Zoom4.jpg" />
+    <img class="image" data-index="12" data-status="inactive" src="../img/front_effect/OvCa-Zoom2.jpg" />
+    <img class="image" data-index="13" data-status="inactive" src="../img/front_effect/OvCa-Zoom14.jpg" />
+    <img class="image" data-index="14" data-status="inactive" src="../img/front_effect/OvCa-Zoom5.jpg" />
+</div>
+
 <div>
 {% for member in site.data.team_members %}
 {% if member.name == 'Ali Bashashati' %}
--- js/custom.js
+++ js/custom.js
@@ -97,42 +97,47 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-
-
     //Front page image effect
     const images = document.getElementsByClassName("image");
     if (images[0] != undefined) {
         let globalIndex = 0,
         last = { x: 0, y: 0 };
-
+    
+        const container = document.querySelector(".front-effect-container"); // Update with your container class
+        const containerRect = container.getBoundingClientRect(); // Get container's position
+    
         const activate = (image, x, y) => {
-        image.style.left = `${x}px`;
-        image.style.top = `${y}px`;
-        image.style.zIndex = globalIndex;
-
-        image.dataset.status = "active";
-
-        last = { x, y };
-        }
-
+            image.style.left = `${x}px`;
+            image.style.top = `${y}px`;
+            image.style.zIndex = globalIndex;
+    
+            image.dataset.status = "active";
+    
+            last = { x, y };
+        };
+    
         const distanceFromLast = (x, y) => {
-        return Math.hypot(x - last.x, y - last.y);
-        }
-
+            return Math.hypot(x - last.x, y - last.y);
+        };
+    
         const handleOnMove = e => {  
             if (window.scrollY <= 450) {
-                if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+                const mouseX = e.clientX - containerRect.left; // Adjust for container's left offset
+                const mouseY = e.clientY - containerRect.top;  // Adjust for container's top offset
+    
+                if(distanceFromLast(mouseX, mouseY) > (window.innerWidth / 20)) {
                     const lead = images[globalIndex % images.length],
                         tail = images[(globalIndex - 5) % images.length];
-
-                    activate(lead, e.clientX, e.clientY);
-
+    
+                    activate(lead, mouseX, mouseY);
+    
                     if(tail) tail.dataset.status = "inactive";
                     
                     globalIndex++;
                 }
             }
-        }
+        };
+    
         window.onmousemove = e => handleOnMove(e);
         window.ontouchmove = e => handleOnMove(e.touches[0]);
     }
